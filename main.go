152c9e9f5915e9385f20513a6ab4bc8dcf55b04c
drypa <dr.drypa.k@mail.ru>
2025-02-19 20:10:50 +0300

    remove hardcored archive path

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import "book-shelf/scanner"
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+import (
+	"book-shelf/scanner"
+	"os"
+)
 
 func main() {
-	scanner := scanner.NewScanner("/home/drypa/Downloads/")
-	_ = scanner.Scan()
+	s := scanner.NewScanner(os.Args[1])
+	_ = s.Scan()
 }
