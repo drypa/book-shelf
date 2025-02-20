60476e13242c893408b8673deafbb4bdf5ce4d32
drypa <dr.drypa.k@mail.ru>
2025-02-20 22:15:01 +0300

    fix errors found in test samples
    try to parse description only
    skip xml syntax errors in body(not closed tags)

--- format/fb2/fb2.go
+++ format/fb2/fb2.go
@@ -3,6 +3,7 @@ package fb2
 import (
 	"encoding/xml"
 	"fmt"
+	"github.com/pkg/errors"
 	"golang.org/x/text/encoding/charmap"
 	"golang.org/x/text/transform"
 	"io"
@@ -46,13 +47,45 @@ func ReadFb2(path string) (*FictionBook, error) {
 		if strings.ToLower(encoding) == "windows-1251" {
 			return transform.NewReader(input, charmap.Windows1251.NewDecoder()), nil
 		}
+		if strings.ToLower(encoding) == "windows-1252" {
+			return transform.NewReader(input, charmap.Windows1252.NewDecoder()), nil
+		}
+		if strings.ToLower(encoding) == "iso-8859-1" {
+			return transform.NewReader(input, charmap.ISO8859_1.NewDecoder()), nil
+		}
 		return nil, fmt.Errorf("unsupported encoding: %q", encoding)
 	}
 
 	fb := FictionBook{}
-	err = decoder.Decode(&fb)
-	if err != nil {
-		return nil, err
+
+	for {
+		tok, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error decoding XML:", err)
+			return nil, errors.New("error decoding XML")
+		}
+
+		switch se := tok.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "description" {
+				var description Description
+				if err := decoder.DecodeElement(&description, &se); err != nil {
+					return nil, errors.Wrap(err, "error decoding description")
+				}
+				fb.Description = description
+				return &fb, nil
+
+			} else if se.Name.Local == "body" {
+				_ = decoder.Skip()
+			}
+		}
+
+		if err != nil {
+			return nil, err
+		}
 	}
 	return &fb, nil
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,10 @@ require (
 	github.com/pkg/errors v0.9.1
 	golang.org/x/text v0.22.0
 )
+
+require (
+	github.com/antchfx/xmlquery v1.4.4 // indirect
+	github.com/antchfx/xpath v1.3.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	golang.org/x/net v0.33.0 // indirect
+)
