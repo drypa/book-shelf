8d8e65e97019a3636de7cff45992ec97bc270589
drypa <dr.drypa.k@mail.ru>
2025-02-22 21:07:46 +0300

    test fix

--- format/fb2/fb2_test.go
+++ format/fb2/fb2_test.go
@@ -1,16 +1,32 @@
 package fb2
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func IgnoreTestReadFb2(t *testing.T) {
+func _TestReadAllFb2(t *testing.T) {
+	dir := "/home/drypa/Downloads/fb2-074392-091839/"
+	files, _ := os.ReadDir(dir)
+	for _, file := range files {
+		fmt.Println(filepath.Join(dir, file.Name()))
+		fb2, err := ReadFb2(filepath.Join(dir, file.Name()))
+		if err != nil || fb2 == nil {
+			t.Fatal(err, file.Name())
+		}
+	}
+}
 
-	fb2, err := ReadFb2("/home/drypa/Downloads/fb2-030560-060423/30569.fb2")
+func _TestReadFb2(t *testing.T) {
+
+	fb2, err := ReadFb2("/home/drypa/Downloads/fb2-074392-091839/91582.fb2")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if fb2.Description.TitleInfo.BookTitle == "" {
-		t.Fatal(fb2.Description.TitleInfo)
+	if fb2.TitleInfo.BookTitle == "" {
+		t.Fatal(fb2.TitleInfo)
 	}
+
 }
